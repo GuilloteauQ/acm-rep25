9a93bde9cb1378a880efd34a541fa3296405f251
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2025-03-24 17:02:43 +0100

    [nix] add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,28 +1,26 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/24.11-pre";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            gnumake
-            texliveFull
-            rubber
-            (rWrapper.override { packages = [ rPackages.tidyverse ]; })
-            graphviz
-			wget
-		    (aspellWithDicts (d: [d.en]))
-          ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              gnumake
+              texliveFull
+              rubber
+              (rWrapper.override { packages = [ rPackages.tidyverse ]; })
+              graphviz
+              wget
+              (aspellWithDicts (d: [ d.en ]))
+            ];
+          };
         };
-      };
-    };
+      }
+	);
 }
